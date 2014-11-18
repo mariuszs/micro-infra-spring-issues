022c67fdf3ca699d62c53b16eb373d52869035e4
Mariusz Smykuła <mariuszs@gmail.com>
2014-11-18 22:31:51 +0100

    fixes

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -1,16 +1,20 @@
 package demo;
 
 import com.ofg.infrastructure.base.MvcWiremockIntegrationSpec;
+import com.ofg.infrastructure.base.MvcWiremockIntegrationTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class) // FIXME: Very ugly hack
 @IntegrationTest
-public class ApplicationTests extends MvcWiremockIntegrationSpec {
+public class ApplicationTests extends MvcWiremockIntegrationTest {
 
 	@Test
 	public void contextLoads() {
